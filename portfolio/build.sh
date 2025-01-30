60f4515eaf332e172cb60ecd6a690121f4ab6773
MuhammadDev <m.awais.softwareeng@gmail.com>
2025-01-30 18:27:45 +0500

    hlo

---
+++ portfolio/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Exit on any error
+set -o errexit  
+
+# Install dependencies
+pip install --upgrade pip  
+pip install -r requirements.txt  
+
+# Run database migrations
+python manage.py migrate  
+
+# Collect static files
+python manage.py collectstatic --noinput  
